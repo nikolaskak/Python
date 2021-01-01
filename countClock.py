57705e043e67dfc6cf9f2e270f8f0bf89b479ebc
nikolaskak <39291270+nikolaskak@users.noreply.github.com>
2021-01-01 16:20:39 +0200

    Update countClock.py

--- countClock.py
+++ countClock.py
@@ -1,13 +1,17 @@
 import time
 
-t=time.localtime()
+t = time.localtime()
+print(t)
+print()
+print(t.tm_hour)
 
-
-newYear = False
 while True:
-    t=time.localtime()
+    t = time.localtime()
+    print(t.tm_year)
     print(t.tm_hour, ":", t.tm_min, ":", t.tm_sec)
     print()
-    if(t.tm_min==3):
+    if(t.tm_year == t.tm_year+1):
+        if (t.tm_hour == 00 and t.tm_min == 00 and t.tm_sec == 00):
+            print("Happy New Year!!")
+            break
 
-        print("Happy New Year!!")
